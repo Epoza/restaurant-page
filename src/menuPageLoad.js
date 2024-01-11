30b78e2997bdf6cf6e494ef887eee5f0ca768667
Epoza <86025026+Epoza@users.noreply.github.com>
2024-01-11 14:13:55 -0500

    finish menu page

--- src/menuPageLoad.js
+++ src/menuPageLoad.js
@@ -3,22 +3,23 @@ export default function menuPageLoad() {
     const navbar = document.createElement("ul");
     navbar.setAttribute("id", "navbar");
 
-    const navbarInfo = document.createElement("li");
-    navbarInfo.setAttribute("id", "home");
-    navbarInfo.style.cursor = "pointer";
-    navbarInfo.textContent = "Info";
+    const navbarHome = document.createElement("li");
+    navbarHome.setAttribute("id", "home");
+    navbarHome.style.cursor = "pointer";
+    navbarHome.textContent = "Home";
 
     const navbarMenu = document.createElement("li");
     navbarMenu.setAttribute("id", "menu");
     navbarMenu.classList.add("active");
     navbarMenu.textContent = "Menu";
+    navbarMenu.style.backgroundColor = '#22a0df';
 
     const navbarContact = document.createElement("li");
     navbarContact.setAttribute("id", "contact");
     navbarContact.style.cursor = "pointer";
     navbarContact.textContent = "Contact";
 
-    navbar.appendChild(navbarInfo);
+    navbar.appendChild(navbarHome);
     navbar.appendChild(navbarMenu);
     navbar.appendChild(navbarContact);
 
@@ -34,7 +35,7 @@ export default function menuPageLoad() {
     const menuItemContainer = document.createElement("div");
     menuItemContainer.setAttribute("id", "meals");
 
-    //Menu item 1
+    // Menu item 1
     const menuItem1 = document.createElement("div");
     menuItem1.classList.add("meal");
     const menuItem1Title = document.createElement("h4");
@@ -47,9 +48,37 @@ export default function menuPageLoad() {
     menuItem1.appendChild(menuItem1Title);
     menuItem1.appendChild(menuItem1Description);
 
-    //add more menu items 2
+    // Menu item 2
+
+    const menuItem2 = document.createElement("div");
+    menuItem2.classList.add("meal");
+    const menuItem2Title = document.createElement("h4");
+    menuItem2Title.classList.add("mealTitle");
+    menuItem2Title.textContent = "Fried Catfish";
+    const menuItem2Description = document.createElement("p");
+    menuItem2Description.classList.add("mealDescription");
+    menuItem2Description.textContent = "Deepfried catfish fillets, delicately seasoned and served with a side coleslaw."
+
+    menuItem2.appendChild(menuItem2Title);
+    menuItem2.appendChild(menuItem2Description);
+
+    // Menu item 3
+
+    const menuItem3 = document.createElement("div");
+    menuItem3.classList.add("meal");
+    const menuItem3Title = document.createElement("h4");
+    menuItem3Title.classList.add("mealTitle");
+    menuItem3Title.textContent = "Fish tacos";
+    const menuItem3Description = document.createElement("p");
+    menuItem3Description.classList.add("mealDescription");
+    menuItem3Description.textContent = "One of our fan favorites, a mix of all our lake fish blended into two tacos, enjoy!"
+
+    menuItem3.appendChild(menuItem3Title);
+    menuItem3.appendChild(menuItem3Description);
 
     menuItemContainer.appendChild(menuItem1);
+    menuItemContainer.appendChild(menuItem2);
+    menuItemContainer.appendChild(menuItem3);
 
     const div = document.createElement("div");
 
