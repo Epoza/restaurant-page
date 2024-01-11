9849e2c2c5dffeed81dcceb6fa0c6119cbc37d7b
Epoza <86025026+Epoza@users.noreply.github.com>
2024-01-11 14:14:29 -0500

    change info to home

--- src/homePageLoad.js
+++ src/homePageLoad.js
@@ -3,10 +3,11 @@ export default function homePageLoad() {
     const navbar = document.createElement("ul");
     navbar.setAttribute("id", "navbar");
 
-    const navbarInfo = document.createElement("li");
-    navbarInfo.setAttribute("id", "home");
-    navbarInfo.textContent = "Info";
-    navbarInfo.classList.add("active");
+    const navbarHome = document.createElement("li");
+    navbarHome.setAttribute("id", "home");
+    navbarHome.textContent = "Home";
+    navbarHome.classList.add("active");
+    navbarHome.style.backgroundColor = '#22a0df';
 
     const navbarMenu = document.createElement("li");
     navbarMenu.setAttribute("id", "menu");
@@ -18,7 +19,7 @@ export default function homePageLoad() {
     navbarContact.style.cursor = "pointer";
     navbarContact.textContent = "Contact";
 
-    navbar.appendChild(navbarInfo);
+    navbar.appendChild(navbarHome);
     navbar.appendChild(navbarMenu);
     navbar.appendChild(navbarContact);
 
@@ -29,7 +30,7 @@ export default function homePageLoad() {
 
     const textEl = document.createElement("p");
     textEl.classList.add("text");
-    textEl.textContent = "Enjoy a tranquil meal by the East Barron Lake. With fresh caught fish and daily specials, stop by today and let us serve you with a fresh meal!";
+    textEl.textContent = "Welcome to Barron Lake Eatery, where culinary excellence meets lakeside charm. Nestled on the shores of the serene Barron Lake, our restaurant invites you to embark on a gastronomic journey that harmonizes flavors with breathtaking views. At Barron Lake Eatery, we take pride in offering a diverse menu that celebrates both local and international cuisines, curated with the finest, freshest ingredients. From mouthwatering seafood delicacies to hearty comfort foods, each dish is a masterpiece crafted by our skilled chefs. Our inviting ambiance, inspired by the natural beauty of the lake, provides the perfect backdrop for a memorable dining experience. Whether you're enjoying a romantic evening by the water, gathering with friends, or savoring a family meal, Barron Lake Eatery promises an unparalleled fusion of taste, atmosphere, and hospitality. Come join us, and let Barron Lake Eatery be your destination for exceptional dining moments by the lake.";
 
     const div = document.createElement("div");
 
